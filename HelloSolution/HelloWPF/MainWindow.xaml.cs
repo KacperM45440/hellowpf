0de144eef96cf8854f60ac09031be9adeca2b8b2
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-10-04 20:23:37 +0200

    Basic buttons

--- HelloSolution/HelloWPF/MainWindow.xaml.cs
+++ HelloSolution/HelloWPF/MainWindow.xaml.cs
@@ -16,9 +16,46 @@ namespace HelloWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Button newButton;
+        private WrapPanel buttonPanel;
+        private TextBlock buttonText;
         public MainWindow()
         {
+            newButton = new();
+            buttonPanel = new();
+            buttonText = new();
+
             InitializeComponent();
+            CreateButton();
+        }
+
+        private void CreateButton()
+        {
+            newButton.FontWeight = FontWeights.Bold;
+            newButton.Margin = new Thickness(440, 300, 160, 50);
+
+            for (int i=0; i<3; i++)
+            {
+                buttonText = new();
+                switch(i)
+                {
+                    case 0:
+                        buttonText.Text = "Multi";
+                        buttonText.Foreground = Brushes.Blue;
+                        break;
+                    case 1:
+                        buttonText.Text = "Color";
+                        buttonText.Foreground = Brushes.Red;
+                        break;
+                    case 2:
+                        buttonText.Text = "C#Button";
+                        break;
+                }
+                buttonPanel.Children.Add(buttonText);
+            }
+
+            newButton.Content = buttonPanel;
+            gridMain.Children.Add(newButton);
         }
     }
 }
